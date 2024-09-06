6e6ea3ec3f371b87052beb645fe35d1d39c87a25
Pitiphong Kitrueangphathchara <Pitiphong.kit@gmail.com>
2024-09-06 14:45:45 +0700

    add session.php

--- index.php
+++ index.php
@@ -2,26 +2,8 @@
 // Create database connection
 include 'connection.php'; 
 
-// Start session
-session_start();
-
 // Create a new session for first-time users
-if (!isset($_SESSION["session_id"])) {
-
-    // Prepare query, bind parameter, and execute
-    $stmt = $conn->prepare("INSERT INTO game_sessions (session_identifier)
-                            VALUES (?)"); // Prepare query
-    $stmt->bind_param("s", $session_identifier); // bind parameter
-    $session_identifier = session_id(); // Get session id
-    $res = $stmt->execute(); // Execute query
-    if ($res === false) {
-        echo "Could not execute query";
-    }
-
-    // Set to the last auto-increment id
-    $_SESSION["session_id"] = $conn->insert_id; 
-    $stmt->close(); // Close statment
-}
+include 'session.php';
 
 // Close database connection
 $conn->close(); 
